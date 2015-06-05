ece0b179dd4a95d00151e2c1d0a85ea40a171d36
Rick Wright <RickIsWright@live.com>
2015-06-05 09:24:01 -0700

    AspNet Identity 2.1 for MVC and ASP.NET sites
    Working Project for Asp.Net and MVC using the latest version of Identity
    Framework

---
+++ IdentitySample/Account/ResetPassword.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using IdentitySample;
+using IdentitySample.Models;
+using Microsoft.Owin.Security;
+
+namespace IdentitySample.Account
+{
+    public static class IdentityHelper
+    {
+        // Used for XSRF when linking external logins
+        public const string XsrfKey = "XsrfId";
+
+        public static void SignIn(ApplicationUserManager manager, ApplicationUser user, bool isPersistent)
+        {
+            IAuthenticationManager authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            authenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+            var identity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+            authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, identity);
+        }
+
+        public const string ProviderNameKey = "providerName";
+        public static string GetProviderNameFromRequest(HttpRequest request)
+        {
+            return request.QueryString[ProviderNameKey];
+        }
+
+        public const string CodeKey = "code";
+        public static string GetCodeFromRequest(HttpRequest request)
+        {
+            return request.QueryString[CodeKey];
+        }
+
+        public const string UserIdKey = "userId";
+        public static string GetUserIdFromRequest(HttpRequest request)
+        {
+            return HttpUtility.UrlDecode(request.QueryString[UserIdKey]);
+        }
+
+        public static string GetResetPasswordRedirectUrl(string code)
+        {
+            return "/Account/ResetPassword.aspx?" + CodeKey + "=" + HttpUtility.UrlEncode(code);
+        }
+
+        public static string GetUserConfirmationRedirectUrl(string code, string userId)
+        {
+            return "/Account/Confirm.aspx?" + CodeKey + "=" + HttpUtility.UrlEncode(code) + "&" + UserIdKey + "=" + HttpUtility.UrlEncode(userId);
+        }
+
+        private static bool IsLocalUrl(string url)
+        {
+            return !string.IsNullOrEmpty(url) && ((url[0] == '/' && (url.Length == 1 || (url[1] != '/' && url[1] != '\\'))) || (url.Length > 1 && url[0] == '~' && url[1] == '/'));
+        }
+
+        public static void RedirectToReturnUrl(string returnUrl, HttpResponse response)
+        {
+            if (!String.IsNullOrEmpty(returnUrl) && IsLocalUrl(returnUrl))
+            {
+                response.Redirect(returnUrl);
+            }
+            else
+            {
+                response.Redirect("~/Default.aspx");
+            }
+        }
+    }
+    public partial class ResetPassword : Page
+    {
+        protected string StatusMessage
+        {
+            get;
+            private set;
+        }
+
+        protected void Reset_Click(object sender, EventArgs e)
+        {
+            string code = IdentityHelper.GetCodeFromRequest(Request);
+            if (code != null)
+            {
+                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+                var user = manager.FindByName(Email.Text);
+                if (user == null)
+                {
+                    ErrorMessage.Text = "No user found";
+                    return;
+                }
+                var result = manager.ResetPassword(user.Id, code, Password.Text);
+                if (result.Succeeded)
+                {
+                    Response.Redirect("~/Account/ResetPasswordConfirmation.aspx");
+                    return;
+                }
+                ErrorMessage.Text = result.Errors.FirstOrDefault();
+                return;
+            }
+
+            ErrorMessage.Text = "An error has occurred";
+        }
+    }
+}
